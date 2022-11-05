e329cc7a2876c7811050bce21ac6d8a5a831b6ef
Loïc Morvan <loic.morvan.fr@gmail.com>
2022-11-05 23:11:17 +0100

    Using ActivatorUtilities to inject additional dependencies

--- Shell/Implementations/VmFactory.cs
+++ Shell/Implementations/VmFactory.cs
@@ -1,11 +1,21 @@
 namespace Shell.Implementations;
 
-using System;
 using Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
 internal class VmFactory : IVmFactory
 {
+    private readonly IServiceProvider serviceProvider;
+
+    public VmFactory(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
     public IWorkspaceVm CreateWorkspaceVm(Guid id)
     {
-        return new WorkspaceVm(id);
+        // How to retrieve WorkspaceVm from IWorkspaceVm?
+        // serviceProvider should know the component type, but how to get it?
+        return ActivatorUtilities.CreateInstance<WorkspaceVm>(serviceProvider, id);
     }
 }
