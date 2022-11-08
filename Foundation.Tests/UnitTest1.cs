1f346f5dc305f39785a380e16d87ecedd9ac29d2
Loïc Morvan <loic.morvan.fr@gmail.com>
2022-11-08 08:19:00 +0100

    Started to add IL dynamic implementation.

---
+++ Foundation.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Foundation.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Foundation/FactoryBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace Foundation;
+
+using System;
+// using System.Reflection;
+// using System.Reflection.Emit;
+
+public static class FactoryBuilder
+{
+    public static TFactory BuildFactory<TFactory>()
+    {
+        throw new NotImplementedException();
+    }
+}
