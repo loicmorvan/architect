9fcd2542edc2e8d3c1482fe37be5bf28606896fb
Loïc Morvan <loic.morvan.fr@gmail.com>
2022-11-11 01:08:16 +0100

    FactoryCreator works!

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -1,34 +1,21 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Foundation;
-
-System.Console.WriteLine("Started");
+using System.Reflection.Emit;
 
 var services = new ServiceCollection();
 services.AddTransient<IService, Component>();
 services.AddTransient<IOtherService, OtherComponent>();
 
-System.Console.WriteLine("Configured");
-
 var provider = services.BuildServiceProvider();
-
-System.Console.WriteLine("Built");
-
 var factory = FactoryCreator.Create<IFactory>(services, provider);
 
-System.Console.WriteLine("Factory created");
-
 factory.CreateService(54, 54f);
-
-System.Console.WriteLine("Created service");
-
 factory.CreateOtherService("salut");
-
-System.Console.WriteLine("Created other service");
 
 public class OtherComponent: IOtherService
 {
     public OtherComponent(string value)
     {
-        
+
     }
 }
--- Foundation/FactoryCreator.cs
+++ Foundation/FactoryCreator.cs
@@ -41,51 +41,47 @@ public static class FactoryCreator
 
     private static void ImplementCreateMethod(TypeBuilder typeBuilder, MethodInfo info, Type componentType, FieldBuilder serviceProviderField)
     {
-        System.Console.WriteLine("Creating method " + info.Name);
-
         var parameters = info.GetParameters().Select(x => x.ParameterType).ToArray();
-        System.Console.WriteLine(parameters.Select(x => x.ToString()).Aggregate((x, y) => x + ", " + y));
-
         var builder = typeBuilder.DefineMethod(info.Name, MethodAttributes.Public | MethodAttributes.Virtual, info.ReturnType, parameters);
         var gen = builder.GetILGenerator();
 
         var localArray = gen.DeclareLocal(typeof(object[]));
 
         // create and store all parameters in an array
-        gen.EmitWriteLine("Creating arguments array");
         gen.Emit(OpCodes.Ldc_I4, parameters.Length);
         gen.Emit(OpCodes.Newarr, typeof(object));
-        gen.Emit(OpCodes.Stloc, localArray.LocalIndex); // stores the array instance
+        gen.Emit(OpCodes.Stloc, localArray); // stores the array instance
         for (int i = 0; i < parameters.Length; ++i)
         {
-            gen.Emit(OpCodes.Ldloc, localArray.LocalIndex); // array instance
+            gen.Emit(OpCodes.Ldloc, localArray); // array instance
             gen.Emit(OpCodes.Ldc_I4, i); // index in the array
             gen.Emit(OpCodes.Ldarg, i + 1); // object to store
-            gen.Emit(OpCodes.Stelem, parameters[i]); // do store the object into the arry at the given index
+            if (parameters[i].IsValueType)
+            {
+                gen.Emit(OpCodes.Box, parameters[i]);
+            }
+            gen.Emit(OpCodes.Stelem_Ref); // do store the object into the arry at the given index
         }
 
         // push serviceProvider to the stack
-        gen.EmitWriteLine("push serviceProvider to the stack");
         gen.Emit(OpCodes.Ldarg_0);
         gen.Emit(OpCodes.Ldfld, serviceProviderField);
 
         // push componentType to the stack
-        gen.EmitWriteLine("push componentType to the stack");
         gen.Emit(OpCodes.Ldtoken, componentType);
         gen.Emit(OpCodes.Call, typeof(Type).GetMethod("GetTypeFromHandle") ?? throw new Exception("typeof not found?"));
 
         // push array
-        gen.EmitWriteLine("push array");
-        gen.Emit(OpCodes.Ldloc, localArray.LocalIndex);
+        gen.Emit(OpCodes.Ldloc, localArray);
 
         // call ActivatorUtilities.CreateInstance
-        gen.EmitWriteLine("call ActivatorUtilities.CreateInstance");
-        gen.Emit(
-            OpCodes.Call,
-            typeof(ActivatorUtilities).GetMethod("CreateInstance", new[] { typeof(IServiceProvider), typeof(Type), typeof(object[]) })
-                ?? throw new Exception("method ActivatorUtilities.CreateInstance not found"));
+        var method = typeof(ActivatorUtilities)
+                .GetMethod(
+                    "CreateInstance",
+                    new[] { typeof(IServiceProvider), typeof(Type), typeof(object[]) })
+                ?? throw new Exception("method ActivatorUtilities.CreateInstance not found");
+        gen.Emit(OpCodes.Call, method);
 
-        gen.EmitWriteLine("return");
         gen.Emit(OpCodes.Ret);
     }
 }
